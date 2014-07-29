f38f03d0e60e58cde2c4afc08aa55cd9978db6f6
edgarsilva <edgar.osc@gmail.com>
2014-07-29 14:08:05 -0500

    Initial commit, WIP on audio module.

---
+++ test/specs/adaptor.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var Audio = source("adaptor");
+
+describe("Cylon.Adaptors.Audio", function() {
+  var adaptor = new Audio();
+
+  it("needs tests");
+});
---
+++ test/specs/driver.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Audio = source("driver");
+
+describe("Cylon.Drivers.Audio", function() {
+  var driver = new Audio({
+    device: { connection: 'connect' }
+  });
+
+  it("needs tests");
+});
