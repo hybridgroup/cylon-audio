a0d96635777f90280d8a7067530a958cd1120e3f
edgarsilva <edgar.osc@gmail.com>
2014-07-29 14:27:40 -0500

    WIP: Added simplest of playback examples.

---
+++ examples/simple-playback.js
@@ -0,0 +1,17 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connection: { name: 'audio', adaptor: 'audio' },
+  device: {name: 'audio', driver: 'audio'},
+
+  work: function(my) {
+
+    my.audio.on('playing', function(song){
+      console.log('Playing this nice tune: "' + song + '"');
+    });
+
+    // You can also pass a string with a full or relative path here,
+    // in case you do not want to define a list of tracks for the adaptor.
+    my.audio.play('./audio_samples/audio-sample.mp3');
+  }
+}).start(); 
